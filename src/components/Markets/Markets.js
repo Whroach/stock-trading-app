c236d9ccfc3252471a1f8b1ccb0b1ab3af0d78cd
whroach259@gmail.com <whroach259@gmail.com>
2020-09-06 14:35:54 -0500

    axios request to all quotes to see if it resolves hosting issue

--- src/components/Markets/Markets.js
+++ src/components/Markets/Markets.js
@@ -1,8 +1,9 @@
-import React, { Fragment} from 'react'
+import React, { Fragment, useState, useEffect} from 'react'
 import Equity from './Equity/Equity'
 import './Markets.css'
 import gql from 'graphql-tag'
 import { useQuery } from 'react-apollo'
+import Axios from 'axios'
 
 
 const EQUITIES_QUERY = gql`
@@ -23,17 +24,34 @@ const EQUITIES_QUERY = gql`
 
 export default function Markets(props) {
 
-    const {loading, error, data} = useQuery(EQUITIES_QUERY)
+    const [equities, setEquities] = useState({})
 
-    if(loading) return <div style={{height: "100vh", width: "100vw"}}><p>Loading....</p></div>
-    if(error) console.log(error)
 
-    
+
+    // const {loading, error, data} = useQuery(EQUITIES_QUERY)
+
+    //   if(loading) return <div style={{height: "100vh", width: "100vw"}}><p>Loading....</p></div>
+    //   if(error) console.log(error)
+
+
+    useEffect(() => {
+
+      getQuotes()
+
+    }, [])
+
+    const getQuotes = async () =>{
+      await Axios.get('api/quotes')
+      .then( res => setEquities(res.data))
+      .catch(error => console.log(error))
+    }
+
+
     return (
         <Fragment>
             <div className="body-m" >
                 <div>
-                  <Equity value={data}/>
+                  <Equity value={equities}/>
                 </div>
             </div>
         </Fragment>
